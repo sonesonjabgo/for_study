5f11a6bfd6a364ab5a1efe549e9256919a666a14
JaeHyung <thswogud02@naver.com>
2023-01-18 17:53:13 +0900

    230118 commit 2

--- vscode/과제/5-2hw.py
+++ vscode/과제/5-2hw.py
@@ -13,23 +13,27 @@
 # 윤년이면 > 윤년 출력문 + 연도 입력 재진행
 # 아니면 캘린더 출력 (캘린더 모듈 활용)
 import calendar as cal
-print(cal.isleap(2016))
 
 while True:
     year = int(input('몇 년도 입니까? : '))
     if cal.isleap(year):
         print('윤년입니다. 연도를 다시 입력해주세요.')
-        continue
+        
     else:
         # 여기에 캘린더 출력이 들어가야 함
-        print(cal.prcal(year))
+        print(cal.calendar(year))
         break
 
 # 월 입력
 # 일 입력
 # 몇월 몇일이 월요일인지 파악 (캘린더 모듈 활용)
 # 월요일이면 > 경고문 출력
-month = input()
+month = int(input('월 입력 : '))
+day = int(input('일 입력 : '))
+weekday = cal.weekday(year, month, day)
+if weekday == 0:
+    print('경고 월요일입니다.')
 # 입력받은 연,월,일 딕셔너리에 넣어서 출력
-
-
+weekday_dict = {0:'월요일', 1:'화요일', 2:'수요일', 3:'목요일', 4:'금요일', 5:'토요일', 6:'일요일'}
+my_dict = {'년': year, '월': month, '일': day, '요일': weekday_dict[weekday]}
+print(my_dict)
