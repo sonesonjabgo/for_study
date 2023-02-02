3346feccdde9a83ab352b0c48436d4a720f0e8bc
Jaehyung <thswogud02@naver.com>
2023-02-03 02:18:44 +0900

    230202 commit at home

--- vscode/과제/elec_bus.py
+++ vscode/과제/elec_bus.py
@@ -12,19 +12,21 @@ T = int(input()) # 3
 for t in range(T):
     K, N, M = map(int, input().split())  # 3 10 5
     busstopnum = list(map(int, input().split()))  # [1 3 5 7 9]
+    empty = [0] * 10
+    for i in busstopnum:
+        empty[i] = 1     # [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
     charge = 0
-    mynum = K
-    for n in range(N): # 1 2 3 4 5 6 7 8 9
-        if n in busstopnum:
-            for num in range(M-1): # 0 1 2 3
-                if mynum - (busstopnum[num+1] - busstopnum[num]) < 0: # 남은 이동 거리 - 다음 충전기까지의 거리
-                    charge += 1
-                    mynum = K
-        print(n, mynum)
-        mynum -= 1
-    # if mynum < 0:
-    #     charge = 0
+    idx = 0
+    left = K
+    # print(busstopnum[-1])
+    for n in range(len(N)):
+        if idx >= N:
+            break
+        # left를 하나 소비시키고 idx에 하나 추가시킬 것임
+        left -= 1  # 2
+        idx += 1  # 1
+        if empty[idx] == 1: # idx가 충전소의 위치에 왔을 때
+            if left - (busstopnum[1] - busstopnum[0]) < 0: # 충전을 할지 안할지의 여부가 들어가야 함
 
-    print(f'답은 {mynum} {charge}')
 
 
