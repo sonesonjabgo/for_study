3ae9639697d872864c07382a7f99513e347c7f11
Jaehyung <thswogud02@gmail.com>
2023-02-15 01:01:29 +0900

    230214 commit at home

--- vscode/과제/230213/파스칼의 삼각형/파스칼.py
+++ vscode/과제/230213/파스칼의 삼각형/파스칼.py
@@ -1,14 +1,12 @@
-#import sys
-#sys.stdin = open("input.txt", "r")
+# import sys
+# sys.stdin = open("input.txt", "r")
 
 T = int(input())
 for test_case in range(1, T + 1):
     N = int(input())
+    print(f'#{test_case}')
     for i in range(N):
-        num = 2**i
-        result = '1'
-        while num != 1:
-            num = num - i
-            result = result + str(i)
-        result = result + '1'
-        print(result)
+        pascal = ['1'] + [str(i)] * i
+        if i >= 2:
+            pascal[-1] = '1'
+        print(' '.join(pascal))
