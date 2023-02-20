ce825843c2ca7c5afef50b1fbaa51a73bf993d48
JaeHyung <thswogud02@naver.com>
2023-02-20 17:58:57 +0900

    230220 commit

--- vscode/과제/test.py
+++ vscode/과제/test.py
@@ -10,28 +10,10 @@
 
 # my_list = [0,0,0,0,0,0,0]
 # # my_list[:4] = 1
-# print(my_list)
-
-# print(list(range(0,11,2)))
-# print(list(range(10,0,-1)))
-# for di in [[0,1] [1,0], [0,-1], [-1,0]]:
-#     print(di)
-# case = list(input().split())
-# tc, N = case[0], case[1]
-# print(tc, N)
-# my_list = '123 123'
-# asc = ' '
-# strda = 'adf'
-# # fib = my_list + asc + strda
-# # print(set(fib.split()))
-# # # arr = [input().split() + [0] for _ in range(3)]
-# # a = [['a','b','c'], ['a','b','c']]
-#
-# # print(''.join(''.join(a)))
-# for i in range(5, 0, -1):
-# #     print(i)
-# mylst = [1,2,3,4,5]
-# mylst.pop(0)
-# mylst.pop(0)
-# print(mylst)
-int('덕배')
+my_list = [5, 8, 3]
+i = 0
+while True:
+    my_list[i] = my_list[i] // 2
+    if my_list[i] == 0:
+        my_list[i] = 1
+    i = (i+1) % len(my_list) # N
---
+++ vscode/수업/230220/queue_선형큐.py
@@ -0,0 +1,56 @@
+# 마치 큐인 것 처럼 동작하는 리스트 만들기
+N = 10
+queue = [0] * N
+
+front = rear = -1
+# 큐의 현재 맨 앞 요소와 맨 뒷 요소의 인덱스를 가지고 있는 변수
+# front : 맨 앞 요소의 -1 인덱스
+# rear : 맨 뒷 요소의 인덱스
+
+
+# enQueue(data) : 큐의 맨 뒤에 data 삽입 
+def enQueue(data):
+    global rear
+    if not is_full(): # 데이터가 가득차지 않은 경우에만 들어가야 하기에
+        rear += 1
+        queue[rear] = data
+    else:
+        raise IndexError('큐가 가득 찼다.')
+# deQueue() : 큐의 맨 앞에서 데이터 반환 및 삭제
+def deQueue():
+    global front
+    if not is_empty():
+        front += 1
+        return queue[front]
+    else:
+        raise IndexError('큐가 비었다.')
+#### 디큐를 하고나면 front가 옮겨가는데 그러면 큐 리스트에는 값이 남아있는건가?
+
+
+# is_empty() : 큐가 비었는지 확인하는 함수
+def is_empty(): # 큐가 비었으면 True를 반환 아니라면 False를 반환
+    # front랑 rear랑 같으면 비어있는거
+    if front == rear:
+        return True
+    return False
+
+
+# is_full() : 큐가 가득 찼는지 확인하는 함수
+def is_full():
+    # 선형 큐에서 full이란, rear가 큐의 마지막 인덱스를 가리키고 있을 때
+    if rear == N-1:
+        return True
+    return False
+
+# A, B, C, D, E
+enQueue('A')
+enQueue('B')
+enQueue('C')
+enQueue('D')
+enQueue('E')
+print(deQueue())
+print(deQueue())
+print(deQueue())
+print(deQueue())
+print(deQueue())
+print(deQueue())
