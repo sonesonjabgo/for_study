b77885d29351e1ed4c8466ce6676939a89eab2a9
JaeHyung <thswogud02@naver.com>
2023-02-28 17:53:18 +0900

    230228 commit

--- vscode/과제/test.py
+++ vscode/과제/test.py
@@ -1,53 +1,7 @@
-# # print(list(range(10, 0, -1)))
-#
-# # name = [1, 2, 3, 4]
-# # # print(name[:3])
-# # mlist = [0, 0, 0]
-# # mlist[0] += 1
-# # print(mlist)
-# # arr = [[0 for _ in range(10)] for _ in range(10)]
-# # print(arr)
-#
-# # my_list = [0,0,0,0,0,0,0]
-# # # my_list[:4] = 1
-# <<<<<<< HEAD
-# my_list = [5, 8, 3]
-# i = 0
-# while True:
-#     my_list[i] = my_list[i] // 2
-#     if my_list[i] == 0:
-#         my_list[i] = 1
-#     i = (i+1) % len(my_list) # N
-# =======
-# # print(my_list)
-#
-# # print(list(range(0,11,2)))
-# # print(list(range(10,0,-1)))
-# # for di in [[0,1] [1,0], [0,-1], [-1,0]]:
-# #     print(di)
-# # case = list(input().split())
-# # tc, N = case[0], case[1]
-# # print(tc, N)
-# # my_list = '123 123'
-# # asc = ' '
-# # strda = 'adf'
-# # # fib = my_list + asc + strda
-# # # print(set(fib.split()))
-# # # # arr = [input().split() + [0] for _ in range(3)]
-# # # a = [['a','b','c'], ['a','b','c']]
-# #
-# # # print(''.join(''.join(a)))
-# # for i in range(5, 0, -1):
-# # #     print(i)
-# # mylst = [1,2,3,4,5]
-# # mylst.pop(0)
-# # mylst.pop(0)
-# # print(mylst)
-# # int('덕배')
-# # my_lst = [0]
-# lst1 = [1]
-# print(lst1 + [2])
-# >>>>>>> 56703781b1f17c0d791973dfc83e31864da374e1
-mys = [1,2]
-letter = 7
-print(letter, *mys)
+# 암호가 5자라면라면룬ㅇ룬ㅇ먀
+
+# 0 1 2 3 4 5 6 7 8 9 10
+
+# range(10, ).
+
+print(min(1, 2, 3, 4))
