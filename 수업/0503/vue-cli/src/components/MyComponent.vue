8ed8875a6ba633cb793f887c6e6a00aab69633ab
Jaehyung <thswogud02@naver.com>
2023-05-03 17:55:09 +0900

    230503

---
+++ 수업/0503/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="border">
+    <h1>This is MyComponent</h1>
+    <MyChild static-props="component(부모)에서 child로"
+    :my-props="dynamicProps"
+    @child-to-parent="parentGetEvent"
+    @child-input="getDynamicData"
+    />
+
+  </div>
+</template>
+
+<script>
+import MyChild from '@/components/MyChild'
+
+export default {
+  name: 'MyComponent',
+  components: {
+    MyChild,
+  },
+  data : function () {
+    return {
+      dynamicProps : "It is a data"
+  }},
+  methods : {
+    parentGetEvent(inputData){
+      console.log('자이자이자식아')
+      console.log(`child에서 ${inputData}를 받음`)
+    },
+    getDynamicData(input) {
+      console.log(`child에서 입력한 ${input}을 출력 !`)
+    }
+  }
+}
+
+</script>
+
+<style>
+  .border {
+    border: solid;
+  }
+</style>
